d7ea5f94a465ad8c9d9a32b6c25f7beba149336a
Henry Pham <hqpham98@gmail.com>
2023-11-14 00:55:01 +0000

    Completed submit form and added beforeunload event listener #1

--- js/data.js
+++ js/data.js
@@ -1,8 +1,14 @@
 /* exported data */
 
-let data = {
+const data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1,
 };
+
+function handleBeforeUnload(event) {
+  localStorage.setItem('taco', JSON.stringify(data));
+}
+
+window.addEventListener('beforeunload', handleBeforeUnload);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* global data */
+
 const $photoURL = document.querySelector('#photo-URL');
 const $photo = document.querySelector('form img');
 const $form = document.querySelector('form');
@@ -8,6 +10,14 @@ function handlePhotoInput(event) {
 
 function handleSubmit(event) {
   event.preventDefault();
+  const inputObj = {};
+  for (let i = 0; i < Array.from(event.target).length - 1; i++) {
+    inputObj[event.target[i].id] = event.target[i].value;
+  }
+  inputObj.entryID = data.nextEntryId++;
+  data.entries.unshift(inputObj.entryID);
+  $form.reset();
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
 }
 $photoURL.addEventListener('input', handlePhotoInput);
 
